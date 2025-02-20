43dcd5e66c1c068c88ee0f01061eb23f4d68f3cc
knivram <129607867+knivram@users.noreply.github.com>
2025-02-20 22:20:08 +0100

    git init basic bluetooth scan function

---
+++ app/src/main/java/com/example/bluetooth_chat/presentation/BluetoothUiState.kt
@@ -0,0 +1,8 @@
+package com.example.bluetooth_chat.presentation
+
+import com.example.bluetooth_chat.domain.chat.BluetoothDevice
+
+data class BluetoothUiState(
+    val scannedDevices: List<BluetoothDevice> = emptyList(),
+    val pairedDevices: List<BluetoothDevice> = emptyList(),
+)
