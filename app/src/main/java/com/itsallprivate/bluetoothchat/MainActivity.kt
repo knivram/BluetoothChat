7fa4a28111087e0ffa22ff568283f42107ea09cf
knivram <129607867+knivram@users.noreply.github.com>
2025-02-24 15:50:28 +0100

    using navigation with default rout

---
+++ app/src/main/java/com/itsallprivate/bluetoothchat/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.itsallprivate.bluetoothchat
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.itsallprivate.bluetoothchat.domain.chat.BluetoothDevice
+import com.itsallprivate.bluetoothchat.presentation.components.BluetoothConnectionScreen
+import com.itsallprivate.bluetoothchat.ui.theme.BluetoothChatTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.serialization.Serializable
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private val bluetoothManager: BluetoothManager? by lazy {
+        applicationContext.getSystemService(BluetoothManager::class.java)
+    }
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val enableBluetoothLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { /* Not needed */ }
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { perms ->
+            val canEnableBluetooth = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                perms[Manifest.permission.BLUETOOTH_CONNECT] == true
+            } else true
+
+            if(canEnableBluetooth && !isBluetoothEnabled) {
+                // ask user to turn on bluetooth
+                enableBluetoothLauncher.launch(
+                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                )
+            }
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            permissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                )
+            )
+        }
+
+        setContent {
+            BluetoothChatTheme {
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = NavRoutes.BluetoothConnection ) {
+                    composable<NavRoutes.BluetoothConnection> {
+                        BluetoothConnectionScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+sealed class NavRoutes {
+    @Serializable
+    object BluetoothConnection: NavRoutes()
+}
