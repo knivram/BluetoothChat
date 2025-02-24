195649734526ddcfc9e7f1e9195d2da784d2d3b3
knivram <129607867+knivram@users.noreply.github.com>
2025-02-25 00:02:09 +0100

    added date time to message, removed the device name and made it serializable to be sent over bluetooth

--- app/src/main/java/com/itsallprivate/bluetoothchat/domain/chat/BluetoothMessage.kt
+++ app/src/main/java/com/itsallprivate/bluetoothchat/domain/chat/BluetoothMessage.kt
@@ -1,7 +1,32 @@
 package com.itsallprivate.bluetoothchat.domain.chat
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Serializable
 data class BluetoothMessage(
     val message: String,
-    val senderName: String,
     val isFromLocalUser: Boolean,
+    @Serializable(with = DateSerializer::class)
+    val dateTime: LocalDateTime,
 )
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = LocalDateTime::class)
+object DateSerializer : KSerializer<LocalDateTime> {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(value.format(formatter))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime{
+        return LocalDateTime.parse(decoder.decodeString(), formatter)
+    }
+}
